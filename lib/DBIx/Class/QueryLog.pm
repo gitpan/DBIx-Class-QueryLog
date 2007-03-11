aafdcdd2ec4cad784f3620311784f38baf75ad7c
Cory G Watson <gphat@cpan.org>
2007-03-11 15:49:27 +0000

    Import of GPHAT/DBIx-Class-QueryLog-0.03 from CPAN.
    gitpan-cpan-distribution: DBIx-Class-QueryLog
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         GPHAT/DBIx-Class-QueryLog-0.03.tar.gz
    gitpan-cpan-author:       GPHAT
    gitpan-cpan-maturity:     released

--- lib/DBIx/Class/QueryLog.pm
+++ lib/DBIx/Class/QueryLog.pm
@@ -17,11 +17,11 @@ DBIx::Class::QueryLog - Log queries for later analysis.
 
 =head1 VERSION
 
-Version 0.01
+Version 0.03
 
 =cut
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 =head1 SYNOPSIS
 
